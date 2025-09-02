ff38ebb6e9b708fe007cdf0ccee4f55e5179ed8f
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-02 11:52:35 +0530

    Create Basics Learning print statemtnt.py

---
+++ Basics Learning print statemtnt.py
@@ -0,0 +1,34 @@
+# to write the print statement in the terminal 
+print("Hey this is me varad")
+print("Hyyy \nhy. \ni am a student")
+# end statement means it end the text with the one you have given 
+
+print("Heyy yoo", end="\n")
+print("\n")
+# write a program to print the following pattern 
+
+# solution -1
+print("*")
+print("**")
+print("***")
+print("****\n" ,end="*****")
+
+print("\n")
+#solution -2
+print("*\n" "**\n" "***\n" "****\n" "*****\n")
+
+#solution -3  using triple quotes 
+print("""
+*
+**
+***
+****
+*****
+      """)
+
+
+# question -2: 
+print("\n     Menu \n" , "Tea     Rs.10\n" , "Coffee  Rs.20\n" , "Juice   Rs.30\n")
+
+
+# for the \t it is know as tab and it created 4 spaces 
