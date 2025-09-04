3905a3249a37157378d566bad74430c310be15e7
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-04 09:55:42 +0530

    Create Form Filler.py

---
+++ Form Filler.py
@@ -0,0 +1,16 @@
+# Form Filler:
+# user enters name , age , marks -> all strings -> convert appropriately 
+# Display a formatted message.
+
+
+name=str(input("Please enter your name "))
+age=int(input("please enter your age "))
+marks=float(input("please enter your marks "))
+
+name = str(name)
+age = str(age)
+marks = str(marks)
+
+print("Name:-",name)
+print("Age:-",age)
+print("Marks:-",marks)
