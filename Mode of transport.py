0c375561c0e092ace3803eb70e90dee83380996c
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-07 14:44:26 +0530

    Create Mode of transport.py

---
+++ Mode of transport.py
@@ -0,0 +1,8 @@
+mode = input("Please enter the preferred mode of transport")
+
+if(mode == ""):
+    print("You did not enter a mode of transport")
+elif(mode != ""):
+    print(f"Your Preferred mode of transport is{mode}")
+
+
