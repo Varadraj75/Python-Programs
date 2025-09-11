4f97e5f7a3289aa6d36b9f06c0543092bc3aa1d5
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-11 13:16:46 +0530

    Create Workout Timer.py

---
+++ Workout Timer.py
@@ -0,0 +1,11 @@
+# Workout timer countdown 
+
+workout_time = int(input())
+
+while workout_time>=0:
+    print(f"Time reaming: {workout_time} seconds")
+    workout_time = workout_time -1
+
+    if(workout_time == 0):
+        print("Great job!!")
+        break
