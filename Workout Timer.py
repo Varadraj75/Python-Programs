5443a8069b233e2df1fda6decf235a160bf95503
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-11 13:29:23 +0530

    Update Workout Timer.py

--- Workout Timer.py
+++ Workout Timer.py
@@ -5,9 +5,6 @@ workout_time = int(input())
 while workout_time>=0:
     print(f"Time reaming: {workout_time} seconds")
     workout_time = workout_time -1
-
-    if(workout_time == 0):
-        print(f"Time reaming: {workout_time} seconds")
-        print("Workout Completed Great job!!")
-        break
+print("Workout Completed Great job!!")
+        
         
