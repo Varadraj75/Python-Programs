e7986914fd268a6ac2c6c01af5bd2e1c061cdbee
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-11 12:54:27 +0530

    Create student attendance tracker.py

---
+++ student attendance tracker.py
@@ -0,0 +1,15 @@
+# Student attendance tracker
+
+name = input("Enter the name").strip()
+total_days = int(input("Enter the days"))
+
+day = 1
+
+while day<=total_days:
+    if day ==3:
+        day += 1
+        continue
+    else:
+        print(f"Attendance recorded for {name} on day {day}")
+        day += 1
+
