711e5503630b15c36575b8248fadec0937c79094
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-12 16:27:19 +0530

    Update String.py

--- String.py
+++ String.py
@@ -52,4 +52,52 @@ p=""
 for i in range(len(s)-1 , -1,-1):
     print(s[i],end="")
 
+print("")
+#Q2:- 
+l="Hello, Python World!"
+print(l[14:19])
+for i in range(1,len(l),2):
+    print(l[i],end="")
 
+print(l[1::2])
+# here 3 is the step size
+print(l[1::3])
+
+print(l[7:len(s)-1])
+
+
+none = "abcdefghij"
+print(none[2:5])
+print(none[-3:])
+print(none[::3])
+
+
+#find() : it gives me the starting index of the word which we are passing in the original string s. 
+
+s= " Hello world " 
+print(s.find('o'))
+
+print(s)
+#Strip() : it removes the spaces from the start and the end
+print(s.strip())
+
+
+# Replace function: replace(s,s2) -> it replace the word  , s-> initial word , s2 -> replace word
+s2 = s.replace('world','python')
+print(s2)
+# if u want to replace only one word  add 1 in last
+s2 = s.replace('wolrd' , 'python',1)
+# if u want to replace only one word  add 2 in last
+s2 = s.replace('wolrd' , 'python',2)
+
+
+
+#Q3:- 
+
+random="   welcome to,  Python programming. Enjoy Coding!   "
+print(random.strip())
+print(random.replace(',' , ';'))
+print(random.find("Python"))
+sp = random.split(" ")
+print(len(sp))
+print(random.split("."))
