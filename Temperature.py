952b33d7109d671d0e3e5e1e1a2db657e20ae0a8
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-12 11:42:14 +0530

    Update Temperature.py

--- Temperature.py
+++ Temperature.py
@@ -7,10 +7,10 @@ reading_2 = round(Temperature_2,3)
 reading_3 = round(Temperature_3,3)
 
 
-reading_average = round((reading_1+reading_2+reading_3)/3,3)
+reading_average = (reading_1+reading_2+reading_3)/3
 average = (Temperature_1+Temperature_2+Temperature_3)/3
 
-diff =round(reading_average - average,3)
+diff =reading_average - average
 
 print(f"Rounded Temperature 1: {reading_1}°C")
 print(f"Rounded Temperature 2: {reading_2}°C")
