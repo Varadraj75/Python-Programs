8b701745a8cc63716b2affd545caa7416dd45d18
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-12 14:58:59 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,18 @@
+# Learning Strings
+
+#indexing
+school ="polaris"
+print(school[2])
+print(school[0])
+print(school[5])
+
+#Concatenation:
+
+print("Raman" +"Bhari"+" 2")
+#print("Raman" +"Bhari"+2) # TypeError
+
+
+#Slicing:
+s = "Aryan"
+w = s[2] + s[3]
+print(w)
