2b02d513ee18a9f757a20c547468dc68f829f876
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-12 11:33:01 +0530

    Create Temperature.py

---
+++ Temperature.py
@@ -0,0 +1,20 @@
+Temperature_1 = 23.87654321
+Temperature_2 = 23.8760
+Temperature_3 = 23.8769
+
+reading_1 = round(Temperature_1,3)
+reading_2 = round(Temperature_2,3)
+reading_3 = round(Temperature_3,3)
+
+
+reading_average = round((reading_1+reading_2+reading_3)/3,3)
+average = (Temperature_1+Temperature_2+Temperature_3)/3
+
+diff =round(reading_average - average,3)
+
+print(f"Rounded Temperature 1: {reading_1}°C")
+print(f"Rounded Temperature 2: {reading_2}°C")
+print(f"Rounded Temperature 3: {reading_3}°C")
+print(f"Average Temperature: {reading_average}°C")
+print(f"Difference in Average Temperature: {diff}°C")
+
