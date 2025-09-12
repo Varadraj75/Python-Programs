ba7c9bd3e663c4ec8f70f26916ce8c8aef6b6a80
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-12 12:04:55 +0530

    Create Amusement park.py

---
+++ Amusement park.py
@@ -0,0 +1,10 @@
+start_level = int(input())
+
+level = 0
+
+while level<=start_level:
+    print(f"ride descending: now at level{start_level}")
+    start_level -=1 
+
+
+print("The ride has safelt reached the ground!") 
