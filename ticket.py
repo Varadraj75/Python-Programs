acfafa8186103a9e2743d60d904a492c1c843184
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-12 10:59:35 +0530

    Create ticket.py

---
+++ ticket.py
@@ -0,0 +1,9 @@
+total_tickets = int(input())
+
+ticket_number =1 
+
+while ticket_number<=total_tickets:
+    print(f"Ticket {ticket_number} issued.")
+    ticket_number +=1
+
+print("All tickets issued. Counter closed.")
