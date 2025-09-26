3d7f1e1c49d8339137b187b85762a37e0316e6d0
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-09-26 10:53:31 +0530

    Leaning file handling writing

--- file_handling.py
+++ file_handling.py
@@ -7,8 +7,14 @@ i = open(r"/Users/varad/Documents/GitHub/Python-Programs/demo.txt" , 'r')
 print(i.readline(1))
 print(i.readline(5))
 print(i.readline())
+# seek will be helpful for moving the cursor to the index value
+i.seek(0)
 # readlines will read all the lines , it creates the list
 print(f.readlines())
 
+# it's important to close the file so that no one can edit the file 
+i.close()
 
 
+j=open("fle.txt",'w')
+j.write('welcome to pst')
