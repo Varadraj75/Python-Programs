14ef5e26903a2b361dea2e9519e3b93c91652ecb
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-11 11:27:47 +0530

    practice for mid_sems

--- practice_for_mid_sems.py
+++ practice_for_mid_sems.py
@@ -10,3 +10,42 @@ for ele in li:
 
 print(min_ele)
 print(max_ele)
+
+
+#Find the duplicates in the list 
+
+l=[1,1,2,2,5,2,6,7,5,3,3]
+
+value=set()
+output=[]
+for ele in l:
+    if(ele in value):
+        output.append(ele)
+    else:
+        value.add(ele)
+
+print(output)
+
+
+#Find all the subarrays:-
+for i in range(0,len(l)):
+    for j in range(i,len(l)):
+        for k in range(i,j+1):
+            print(l[k],end=" ")
+        print()
+
+#Find the maxsumarray
+print("Max-Array")
+l=[1,2,3,4,5,6]
+curr_sum = 0
+max_ele = float('-inf')
+
+for i in range(0,len(l)):
+    curr_sum += l[i]
+
+    if(curr_sum>max_ele):
+        max_ele=curr_sum
+    if(curr_sum<0):
+        curr_sum=0
+
+print(max_ele)
