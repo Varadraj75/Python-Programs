b773a0616f604187c043b784243f97da26cf0985
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-16 22:54:17 +0530

    practice for mid_sems

--- mid_sem.py
+++ mid_sem.py
@@ -1,17 +1,23 @@
-# se={"apple","orange","banana","pineapple"}
+# # se={"apple","orange","banana","pineapple"}
 
-# print(*se)
+# # print(*se)
 
-# f=open('file.txt','r')
-# # print(f.readline(4)) 
-# # f.seek(1)
-# # print(f.readline())
-# print(f.readlines())
+# # f=open('file.txt','r')
+# # # print(f.readline(4)) 
+# # # f.seek(1)
+# # # print(f.readline())
+# # print(f.readlines())
 
-f=open('xyx.txt','a')
-f.write(" something")
-# f.write("people")
-# li=['hey','welcome','pst']
-# f.writelines(li)
+# # f=open('xyx.txt','a')
+# # f.write(" something")
+# # f.write("people")
+# # li=['hey','welcome','pst']
+# # f.writelines(li)
+
+
+# str="Varad raj agrawal"
+
+# for i in range(65, 70):
+#     print((chr(i) + " ") * (i - 64))
 
 
