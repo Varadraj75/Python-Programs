2ba5513d69b8529c372f9a1aa684a1f35cb64d59
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-27 13:03:41 +0530

    leaning exception handling

---
+++ Exception_handling.py
@@ -0,0 +1,17 @@
+#Exception handling 
+while True:
+   
+
+    try:
+        a=int(input())
+        b=int(input())
+        res = a/b
+    # except ZeroDivisionError:
+    #     print("Please don't give the value 0 to denominator")
+        # continue
+    except Exception:
+        print("Some error")
+    else:
+        print(res)
+    finally:
+        print("my code is running well")
