4ddc780254537d4648347400df50a580de47dba5
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-28 11:22:58 +0530

    File_handling using ExceptionHandling

---
+++ Filehandling_ExceptionHandling.py
@@ -0,0 +1,13 @@
+while True:    
+    try:
+        file_name = input("Enter the file name")
+        o = open(file_name,'r')
+    except FileNotFoundError:
+        print("File not found")
+    except PermissionError:
+        print("Permission is denied")
+    except Exception:
+        print("Unexpected error")
+    else:
+        data = o.read()
+        print(data)
