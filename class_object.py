6e8c8a19f0cfcfdaa3c101aa8e9da4049428f0cf
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-29 10:36:04 +0530

    learning Class and object

--- class_object.py
+++ class_object.py
@@ -4,6 +4,8 @@ class student:
     name=""
     age=""
     gender=""
+    def display_info(self):
+        print(self.name , self.roll_no,self.age,self.gender)
 
 s1 = student()
 s1.roll_no=10
@@ -11,3 +13,8 @@ s1.name="hari"
 s1.age=19
 s1.gender="male"
 
+#self refers to the current instance of the class
+
+
+
+s1.display_info()
