6efae3e3789fe6fb18e5ed2a7a946c251faa8818
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-29 15:02:50 +0530

    Constructor

--- Constructor.py
+++ Constructor.py
@@ -13,3 +13,22 @@ class classplus:
 
 pst=classplus("pst","Yellow")
 testbook = classplus("testbook","blue")
+
+class student:
+    #constructor
+    def __init__(self,n,r,a,g):
+        self.name = n
+        self.roll_no = r
+        self.age = a
+        self.gender = g
+
+    #method 1:
+    def display_info(self):
+        print(self.name,self.roll_no,self.age,self.gender)
+
+#main
+s1 = student("kp","4cse04",23,"male")
+s1.display_info()
+
+s2 = student("k","4e04",2,"female")
+s2.display_info()
