da66c51f19b4dfb54dbd151a55830e2df32cbff6
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-29 11:10:23 +0530

    learning Class and object

--- class_object.py
+++ class_object.py
@@ -4,8 +4,16 @@ class student:
     name=""
     age=""
     gender=""
+    #method 1
+    #self refers to the current instance of the class
     def display_info(self):
         print(self.name , self.roll_no,self.age,self.gender)
+    #method 2
+    def set_info(self):
+        self.name = input("Enter the student name")
+        self.roll_no = input("Enter the Roll Number")
+        self.age = input("Enter the Age")
+        self.gender = input("Enter the gender")
 
 s1 = student()
 s1.roll_no=10
@@ -13,8 +21,6 @@ s1.name="hari"
 s1.age=19
 s1.gender="male"
 
-#self refers to the current instance of the class
-
-
-
+s1.set_info()
 s1.display_info()
+
