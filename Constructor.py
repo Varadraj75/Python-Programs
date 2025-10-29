23e8c4917e7ee26abbe3d86698b927cc6e7bb7ff
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-29 11:44:55 +0530

    Constructor

---
+++ Constructor.py
@@ -0,0 +1,14 @@
+# Constructor-> 
+
+class classplus:
+    #attribute
+    brand_color="blue"
+    
+    #defining constructor -> method-constructor
+    def __init__(self,company,color):
+        print(f"The company color for {company} is {color}")
+    
+
+pst=classplus("pst","Yellow")
+testbook = classplus("testbook","blue")
+
