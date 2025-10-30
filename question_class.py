542310a485bb49e56f7e5e9e5c8344574ba9be5b
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-30 11:45:03 +0530

    Exception handling using class

--- question_class.py
+++ question_class.py
@@ -53,3 +53,24 @@ c1.addition(10,20)
 c1.subtraction(10,20)
 c1.multiplication(10,20)
 c1.division(20,10)
+
+#Q4:- BankAccount
+class InsucificentFunds(Exception):
+         pass
+class BankAccount:
+    def __init__(self):
+        self.AccountNumber=int(input("Enter the number"))
+        self.Account_holder_name=input("Enter the name")
+        self.balance=float(input("Enter the balance"))
+    def withdraw(self):
+        self.withdra = float(input("Enter the withdraw amount"))
+        try:
+            if(self.withdra > self.balance):
+                raise InsucificentFunds("It has insucificent funds")
+        except InsucificentFunds as msg:
+            print(msg)
+        else:
+            print("fund is withdraw")
+        
+b1 = BankAccount()
+b1.withdraw()
