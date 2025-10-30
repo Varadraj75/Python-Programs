761d972066fdac32c4797b509258b26629fb2819
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-30 11:14:20 +0530

    static method

--- static_method.py
+++ static_method.py
@@ -1,5 +1,6 @@
 #Static method
 class car:
+    no_wheel = 4
     def __init__(self,branch,price):
         self.brand = branch
         self.price = price
@@ -10,10 +11,14 @@ class car:
     @staticmethod
     def discount_display():
         print("Congrats you will be getting 10% discount")
+    
+    def number_type():
+        print(car.no_wheel)
 
 
 bmw = car("BMW","340298457982")
 bmw.display()
+car.number_type()
 # discount_display() -> error
 bmw.discount_display() # error (no error if we use keyword @staticmethod)
 # car.discount_display()
