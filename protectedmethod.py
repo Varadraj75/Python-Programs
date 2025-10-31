8352369d581336232a41ccb6fa03d35234551ae3
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-10-31 13:01:06 +0530

    protected method

---
+++ protectedmethod.py
@@ -0,0 +1,18 @@
+#protected 
+# it is not accessable for outside of theclass but we access inside the subclass
+class hari:
+    def __init__(self):
+        self._balance=15000
+    
+    def display(self):
+        print(f"Balance of hari is{self.balance}")
+
+
+class Krishna(hari):
+    def display_info(self):
+        print(f"Balance of father is {self._balance}")
+
+obj1 = Krishna()
+obj1.display_info()
+
+
