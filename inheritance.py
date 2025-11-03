5385f820bf2295d8dc4a89d4f838d3ebfbd1ee08
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-03 11:42:34 +0530

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -13,15 +13,21 @@ class vehicle:
         print("Vechile is stopped")
 
 class car(vehicle):
-    pass
+    def __init__(self, b, c,d):
+        super().__init__(b,c,)
+        self.typres = d
+    
+    def no_wheels(self):
+        print(f"Number of wheels {self.typres}")
 
-obj = car(10,20)
+obj = car(10,20,4)
+obj.no_wheels()
 obj.key()
 obj.Break()
 
 #types of inheritance 
-# 1) Single inheritance
-# 2) Multiple inheritance
-# 3) Multi Level inheritance
-# 4) Hierarchical Inheritance
-# 5) Hybrid Inheritance
+# 1) Single inheritance -> parent and child
+# 2) Multiple inheritance -> 2 parent and one child
+# 3) Multi Level inheritance -> grandparent - parent - child
+# 4) Hierarchical Inheritance -> 1 parent 2 child
+# 5) Hybrid Inheritance -> parent - child 1 child 2 - parent (diamond style)
