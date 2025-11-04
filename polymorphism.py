ddf297c75588c4011766d387e20b58b9024e30e2
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-04 16:42:47 +0530

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,19 @@
+# Polymorphism 
+# -> Concept under oops, where function(method) , attribute will behave differently based on the situation 
+# compile time polymorphism (method overloading)
+# Run time polymorephism (method overriding)
+# 1) Compile Time
+
+class parent:
+    def display(self):
+        print("Parent class")
+    
+class child:
+    def display(self):
+        print("child class")
+
+obj_child = child()
+child().display()
+
+obj_parent = parent()
+obj_parent.display()
