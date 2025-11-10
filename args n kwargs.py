e7bc4fb66f7654f476c3f5c707079d004d23a5e8
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-10 13:35:46 +0530

    args and kwargs

--- args n kwargs.py
+++ args n kwargs.py
@@ -1,7 +1,18 @@
 # Args & kwargs function 
 
-def cal_sum():
-    print(sum())
+# args-> it is a special variable it will take all positional arguments 
+# syntax ->  *args
+def cal_sum(*args):
+    print(args) # format will be tuple 
+    print(sum(args)) 
 
 
-cal_sum(10,20,30,40)
+cal_sum(10,20,30,40,60,200)
+
+#kwargs 
+# synatx -> **kwargs
+
+def f_n(k,l,**kwargs):
+    print(kwargs) # format will be dictionary 
+
+f_n(100,200,a=10,b=20,c=90,d=85,e=324,f=234)
