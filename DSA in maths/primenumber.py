1cb2e359e6e4e4c3a9948a6cc5ff4c1b640a1228
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-11 16:17:15 +0530

    Prime number

--- DSA in maths/primenumber.py
+++ DSA in maths/primenumber.py
@@ -1,18 +1,18 @@
 #prime number -> to check the prime number 
 
-# a = int(input("Enter a number to check wheather it is a prime number or not "))
+a = int(input("Enter a number to check wheather it is a prime number or not "))
 
-# flag = True
-# for i in range(2,a):
-#     if(a%i==0):
-#         flag = False
-#         break
+flag = True
+for i in range(2,a):
+    if(a%i==0):
+        flag = False
+        break
 
 
-# if(flag):
-#     print("Prime number")
-# else:
-#     print("Not a prime ")
+if(flag):
+    print("Prime number")
+else:
+    print("Not a prime ")
 
 
 a = int(input("Enter the number "))
@@ -24,4 +24,19 @@ for i in range(2,a+1):
             flag = False
             break
     if flag:
+        print(i)
+
+
+# another approach
+import math
+n=20
+l=[True]*(n+1)
+l[0]=l[1]=False
+
+for i in range(2,int(math.sqrt(n))+1):
+    for j in range(i*i,n+1,i):
+        l[j]=False
+
+for i in range(2,n+1):
+    if(l[i]):
         print(i)
