dc0e4edfaf5c32524d9900643d39b44a7fc14dc5
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-14 13:18:02 +0530

    BUBBLE SORT

---
+++ DSA in maths/bubblesort.py
@@ -0,0 +1,15 @@
+# bubble sort 
+
+arr = [3,9,1,2,29,20]
+
+n = len(arr)
+
+for i in range(0,n):
+    for j in range(0,n-1-i):
+        if(arr[j]>arr[j+1]):
+            temp = arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
+            
+        
+print(arr)
