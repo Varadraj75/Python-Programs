ff0016816a77e0ee7320b96470017349ccd2a24f
Varadraj75 <agrawalvaradraj2007@gmail.com>
2025-11-18 11:08:49 +0530

    User input linked list

--- Linked list/linkedlist.py
+++ Linked list/linkedlist.py
@@ -7,9 +7,13 @@ class node:
 node1 = node(5)
 node2 = node(1)
 node3 = node(4)
+node4 = node(10)
+node5 = node(21)
 
 node1.next = node2
 node2.next = node3
+node3.next = node4
+node4.next = node5
 
 # print(node2.next.data)
 # print(node1.next.next.data)
---
+++ Linked list/userinput_linkedlist.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.next = None
+    
+class Linkedlist:
+    def insertathead():
+        pass
+    def insertatend():
+        pass
+    def insertatpost():
+        pass
+    def display():
+        pass
+
+a = input("Enter the number you want to enter in the node ")
+
+for i in range(a):
+    val = int(input(f"Enter the node{i+1} value"))
+    insertatend(val)
